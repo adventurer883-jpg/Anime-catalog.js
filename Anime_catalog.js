c06a7fc0c9cb688c7af3ed7c4f3b121b9fb6cd1c
adventurer883-jpg <adventurer883@gmail.com>
2026-08-06 09:47:44 +0200

    Refactor anime catalog plugin initialization

--- Anime_catalog.js
+++ Anime_catalog.js
@@ -1,40 +1,44 @@
 (function () {
     'use strict';
 
-    if (window.anime_catalog_plugin) return;
-    window.anime_catalog_plugin = true;
+    function startPlugin() {
+        if (window.anime_catalog) return;
+        window.anime_catalog = true;
 
-    var network = new Lampa.Reguest();
+        function add() {
+            if ($('.menu__item[data-action="anime"]').length) return;
 
-    function addMenu() {
-        if ($('.menu__item[data-action="anime_catalog"]').length) return;
+            var html = '<li class="menu__item selector" data-action="anime">' +
+                '<div class="menu__ico">' +
+                '<svg width="24" height="24" viewBox="0 0 24 24" fill="currentColor"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 14.5v-9l6 4.5-6 4.5z"/></svg>' +
+                '</div>' +
+                '<div class="menu__text">Аниме</div>' +
+                '</li>';
 
-        var item = $('<li class="menu__item selector" data-action="anime_catalog"><div class="menu__ico"><svg width="24" height="24" viewBox="0 0 24 24" fill="currentColor"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 14.5v-9l6 4.5-6 4.5z"/></svg></div><div class="menu__text">Аниме</div></li>');
+            var item = $(html);
 
-        item.on('hover:enter', function () {
-            Lampa.Activity.push({
-                url: '',
-                title: 'Аниме',
-                component: 'category',
-                source: 'tmdb',
-                card_type: true,
-                page: 1,
-                genres: 16,
-                with_original_language: 'ja'
+            item.on('hover:enter', function () {
+                Lampa.Activity.push({
+                    url: '',
+                    title: 'Аниме',
+                    component: 'category',
+                    source: 'tmdb',
+                    page: 1,
+                    genres: '16',
+                    with_original_language: 'ja'
+                });
             });
-        });
 
-        $('.menu .menu__list').eq(0).append(item);
-    }
+            $('.menu .menu__list').eq(0).append(item);
+        }
 
-    function start() {
-        if (window.appready) addMenu();
+        if (window.appready) add();
         else {
             Lampa.Listener.follow('app', function (e) {
-                if (e.type === 'ready') addMenu();
+                if (e.type == 'ready') add();
             });
         }
     }
 
-    start();
+    startPlugin();
 })();
