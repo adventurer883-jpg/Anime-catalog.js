18080d3bbc7d14576189dbf403c17488ff51be70
adventurer883-jpg <adventurer883@gmail.com>
2026-08-06 10:31:29 +0200

    Initialize Anime111 library with core functions

--- Anime.js
+++ Anime.js
@@ -1,1 +1,52 @@
 
+(function () {
+    'use strict';
+
+    if (window.Anime111) return;
+
+    var Anime111 = {
+        version: '0.1.0',
+        name: 'Anime+',
+        modules: {},
+        data: {},
+        cache: {},
+
+        register: function (name, module) {
+            this.modules[name] = module;
+
+            if (typeof module.init === 'function') {
+                module.init();
+            }
+
+            console.log('[Anime111] Module loaded:', name);
+        },
+
+        get: function (name) {
+            return this.modules[name];
+        },
+
+        setData: function (key, value) {
+            this.data[key] = value;
+        },
+
+        getData: function (key) {
+            return this.data[key];
+        },
+
+        cacheSet: function (key, value) {
+            this.cache[key] = value;
+        },
+
+        cacheGet: function (key) {
+            return this.cache[key];
+        }
+    };
+
+    window.Anime111 = Anime111;
+
+    console.log(
+        '%cAnime+ Core loaded',
+        'background:#ff4f81;color:#fff;padding:4px 10px;border-radius:4px;'
+    );
+
+})();
